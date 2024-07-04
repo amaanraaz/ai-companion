82b0495de10624ef0d757b830a55863a20b6b429
amaanraaj <amandwivedi00@gmail.com>
2024-07-05 00:05:53 +0530

    Upgrade Button added with custom variant modified

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { SignInButton, SignedOut } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 const font = Poppins({
     weight: "600",
@@ -27,6 +28,10 @@ const NavBar = () => {
             </Link>
         </div>
         <div className="flex items-center gap-x-3">
+            <Button variant="premium" size="sm">
+                Upgrade
+                <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
+            </Button>
             <SignedOut>
                 <SignInButton/>
             </SignedOut>
